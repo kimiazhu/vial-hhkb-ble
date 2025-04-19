4c41a731f7621c1f902221733f97f0c3a7db7840
kczhu <kczhu@tencent.com>
2025-04-19 22:54:58 +0800

    a keymap solution for win and mac on hhkb-ble

--- keyboards/ydkb/hhkb_ble/keymaps/vial/keymap.c
+++ keyboards/ydkb/hhkb_ble/keymaps/vial/keymap.c
@@ -1,9 +1,10 @@
 #include QMK_KEYBOARD_H
+#include "./helper.h"
 
 #if 0
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {LAYOUT_all(KC_NO)};
 #else
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = 
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
   [0] = {
     { KC_2,   KC_Q,   KC_W,   KC_S,   KC_A,   KC_Z,   KC_X,   KC_C,   },
@@ -15,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     { KC_9,   KC_0,   KC_O,   KC_P,   KC_SCLN,KC_L,   KC_COMM,KC_NO,  },
     { KC_MINS,KC_EQL, KC_RBRC,KC_LBRC,KC_QUOT,KC_SLSH,KC_DOT, KC_CAPS,},
     { KC_LCTL,MO(1),  MO(1),  MO(1),  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,}
-    },  
+    },
   [1] = {
     { KC_F2,  USER00, KC_TRNS,KC_VOLU,KC_VOLD,USER03, KC_TRNS,KC_TRNS,},
     { KC_F3,  KC_F4,  KC_TRNS,USER02, KC_MUTE,KC_TRNS,KC_TRNS,USER01, },
@@ -28,4 +29,128 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     { KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE,KC_VOLD,KC_TRNS,KC_VOLU,}
     },
 };
+
+static uint8_t saved_mods;
+static bool is_direction_down = false;
+static bool is_active = false; // 标记一组快捷键生效，用于按键释放的时候触发 unregister_code
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint8_t held_mods = 0;
+    // 用来标记是否有组合键被激活，如果仅仅是修饰键被按下并释放，这时候应该直接发送给系统，
+    // 比如 GUI 键单独按下释放用于激活系统菜单，SHIFT 单独按下释放用于切换输入法
+    static bool is_combination_active = false;
+    static int curr_layer = 0;
+
+    // 检查是否是修饰键
+    if (keycode >= KC_LCTL && keycode <= KC_RGUI) {
+        if (record->event.pressed) {
+            held_mods |= MOD_BIT(keycode);
+        } else {
+            held_mods &= ~MOD_BIT(keycode);
+
+            if (!is_combination_active) {
+                if (keycode == KC_RALT) {
+                    // RALT 作为一个特殊键，长按加数字用于激活指定层，短按时用于清除所有修饰键和层信息
+                    unregister_mods(MOD_MASK_CSAG);
+                    clear_mods();
+                    layer_clear();
+                    curr_layer = 0;
+                    held_mods = 0;
+                } else {
+                    tap_code(keycode);
+                }
+            }
+
+            if (!held_mods) {
+                // 所有的 mods 组合键都释放了
+                is_combination_active = false;
+            }
+        }
+        return false;
+    } else {
+        if (record->event.pressed && held_mods) {
+            is_combination_active = true;
+            // 在下面定义自己的组合键，2 层是 windows，3 层是 macOS
+            if (0) {}
+            MOVE_to_LAYER(1)
+            MOVE_to_LAYER(2)
+            MOVE_to_LAYER(3)
+            else {
+                if (0) {} // 初始条件，仅为语法占位
+                HANDLE_DOWN_BOTH(KC_H, MOD_BIT(KC_LCTL), KC_LEFT, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_J, MOD_BIT(KC_LCTL), KC_DOWN, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_K, MOD_BIT(KC_LCTL), KC_UP, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_L, MOD_BIT(KC_LCTL), KC_RIGHT, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_H, MOD_BIT(KC_LALT), KC_LEFT, MOD_BIT(KC_LCTL)) // 终端后退一个单词
+                HANDLE_DOWN_BOTH(KC_L, MOD_BIT(KC_LALT), KC_RIGHT, MOD_BIT(KC_LCTL)) // 终端前进一个单词
+                HANDLE_DOWN_BOTH(KC_ESC, MOD_BIT(KC_LSFT), KC_GRV, MOD_BIT(KC_LSFT))
+                HANDLE_DOWN_BOTH(KC_ESC, MOD_BIT(KC_LCTL), KC_GRV, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_BSPC, MOD_BIT(KC_LSFT), KC_BSLS, MOD_BIT(KC_LSFT))
+                HANDLE_DOWN_BOTH(KC_BSPC, MOD_BIT(KC_LCTL), KC_BSLS, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_E, MOD_BIT(KC_LCTL), KC_END, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_QUOT, MOD_BIT(KC_LCTL), KC_BSPC, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_SCLN, MOD_BIT(KC_LCTL), KC_RETN, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_B, MOD_BIT(KC_LCTL), KC_BSPC, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_ESC, MOD_BIT(KC_LALT), KC_GRV, MOD_BIT(KC_LALT))
+                HANDLE_DOWN_BOTH(KC_U, MOD_BIT(KC_LALT), KC_PGUP, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_D, MOD_BIT(KC_LALT), KC_PGDN, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_J, MOD_BIT(KC_LALT), KC_PGUP, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_K, MOD_BIT(KC_LALT), KC_PGDN, MODS_NULL)
+                HANDLE_DOWN_BOTH(KC_D, MOD_BIT(KC_LCTL), KC_DEL, MODS_NULL)
+                HANDLE_DOWN_MAC(KC_R, MOD_BIT(KC_LCTL), KC_R, MOD_BIT(KC_LGUI)) // reload browser
+                HANDLE_DOWN_WIN(KC_A, MOD_BIT(KC_LCTL), KC_HOME, MODS_NULL)
+                HANDLE_DOWN_WIN(KC_A, MOD_BIT(KC_LGUI), KC_A, MOD_BIT(KC_LCTL)) // win+A -> 全选
+                HANDLE_DOWN_WIN(KC_Q, MOD_BIT(KC_LGUI), KC_F4, MOD_BIT(KC_LALT)) // win+Q -> 关闭窗口
+                HANDLE_DOWN_WIN(KC_N, MOD_BIT(KC_LCTL), KC_F2, MODS_NULL) // ctrl+n -> rename
+                else {
+                    register_mods(held_mods);
+                    register_code(keycode);
+                }
+            }
+            return false;
+        } else {
+            if (!record->event.pressed) {
+                if (keycode == MO(1)) {unregister_code(MO(1));layer_move(curr_layer);}
+                HANDLE_UP_BOTH(KC_H, MOD_BIT(KC_LCTL), KC_LEFT, MODS_NULL)
+                HANDLE_UP_BOTH(KC_J, MOD_BIT(KC_LCTL), KC_DOWN, MODS_NULL)
+                HANDLE_UP_BOTH(KC_K, MOD_BIT(KC_LCTL), KC_UP, MODS_NULL)
+                HANDLE_UP_BOTH(KC_L, MOD_BIT(KC_LCTL), KC_RIGHT, MODS_NULL)
+                HANDLE_UP_BOTH(KC_H, MOD_BIT(KC_LALT), KC_LEFT, MOD_BIT(KC_LCTL)) // 终端后退一个单词
+                HANDLE_UP_BOTH(KC_L, MOD_BIT(KC_LALT), KC_RIGHT, MOD_BIT(KC_LCTL)) // 终端前进一个单词
+                HANDLE_UP_BOTH(KC_ESC, MOD_BIT(KC_LSFT), KC_GRV, MOD_BIT(KC_LSFT))
+                HANDLE_UP_BOTH(KC_ESC, MOD_BIT(KC_LCTL), KC_GRV, MODS_NULL)
+                HANDLE_UP_BOTH(KC_BSPC, MOD_BIT(KC_LSFT), KC_BSLS, MOD_BIT(KC_LSFT))
+                HANDLE_UP_BOTH(KC_BSPC, MOD_BIT(KC_LCTL), KC_BSLS, MODS_NULL)
+                HANDLE_UP_BOTH(KC_E, MOD_BIT(KC_LCTL), KC_END, MODS_NULL)
+                HANDLE_UP_BOTH(KC_QUOT, MOD_BIT(KC_LCTL), KC_BSPC, MODS_NULL)
+                HANDLE_UP_BOTH(KC_SCLN, MOD_BIT(KC_LCTL), KC_RETN, MODS_NULL)
+                HANDLE_UP_BOTH(KC_B, MOD_BIT(KC_LCTL), KC_BSPC, MODS_NULL)
+                HANDLE_UP_BOTH(KC_ESC, MOD_BIT(KC_LALT), KC_GRV, MOD_BIT(KC_LALT))
+                HANDLE_UP_BOTH(KC_U, MOD_BIT(KC_LALT), KC_PGUP, MODS_NULL)
+                HANDLE_UP_BOTH(KC_D, MOD_BIT(KC_LALT), KC_PGDN, MODS_NULL)
+                HANDLE_UP_BOTH(KC_J, MOD_BIT(KC_LALT), KC_PGUP, MODS_NULL)
+                HANDLE_UP_BOTH(KC_K, MOD_BIT(KC_LALT), KC_PGDN, MODS_NULL)
+                HANDLE_UP_BOTH(KC_D, MOD_BIT(KC_LCTL), KC_DEL, MODS_NULL)
+                HANDLE_UP_MAC(KC_R, MOD_BIT(KC_LCTL), KC_R, MOD_BIT(KC_LGUI))
+                HANDLE_UP_WIN(KC_A, MOD_BIT(KC_LCTL), KC_HOME, MODS_NULL)
+                HANDLE_UP_WIN(KC_A, MOD_BIT(KC_LGUI), KC_A, MOD_BIT(KC_LCTL)) // win+A -> 全选
+                HANDLE_UP_WIN(KC_Q, MOD_BIT(KC_LGUI), KC_F4, MOD_BIT(KC_LALT)) // win+Q -> 关闭窗口
+                HANDLE_UP_WIN(KC_N, MOD_BIT(KC_LCTL), KC_F2, MODS_NULL) // ctrl+n -> rename
+                else {
+                    unregister_code(keycode);
+                    // 这里之所以清除所有，是为了确保不会因为按键释放顺序导致错误，如果是用比如:
+                    // 如果只是 unregister_mods(held_mods) 的话，假设用户按下 CTRL+C，
+                    // 用户释放比较快，导致 CTRL 先释放了，再释放 C，这时候会导致 CTRL 无法正常释放，因为先释放的
+                    // CTRL 会改变 held_mods 的值。所以这里干脆直接把所有 mods 都 unregister。
+                    unregister_mods(MOD_MASK_CSAG);
+                    clear_mods();
+                    // unregister_mods(held_mods);
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 #endif
--- readme.md
+++ readme.md
@@ -34,3 +34,67 @@ QMK is developed and maintained by Jack Humbert of OLKB with contributions from 
 ## Official Website
 
 [qmk.fm](https://qmk.fm) is the official website of QMK, where you can find links to this page, the documentation, and the keyboards supported by QMK.
+
+# FOR HHKB_BLE
+
+以下是关于 YDKB 固件的说明，修改的位置在 `ydkb/hhkb_ble`，为了限制空间大小，关闭了一些不用的功能，比如奇怪的 combo，mouse 支持等。因为调整了很多组合键，由于原有的GUI 配置工具无法很好支持部分组合键，尤其是 windows 下的 Win 组合键（因为 windows 上这个 GUI 修饰键会有实际作用，不像 mac 上那样），修改了 keymap.c 用于屏蔽不让修饰键在按下的时候就发送给操作系统，从而完美实现各种组合键。
+
+## 编译镜像
+
+`podman pull ghcr.io/kimiazhu/dev-qmk:250412`
+
+也可以选择自己构建，参考：`https://github.com/web3123/dev-containers/tree/main/dev-qmk`，这个镜像内置了 SSH，用户名 root，默认密码为 abc@123456
+
+## 运行编译容器
+
+`pm run -d --user root --cap-add=NET_RAW -v ~/Development:/root/Development -p8890:22  ghcr.io/kimiazhu/dev-qmk:250412`
+
+## 构建固件
+
+通过 ssh 进入容器：`ssh -p 8890 root@localhost`，进入 `/root/Development` 目录，然后 clone 本工程，在工程根目录下执行：
+
+`make ydkb/hhkb_ble:vial`
+
+最后会得到大概如下的输出，表示编译成功：
+![编译成功](images/build-output.jpg)
+
+## 刷写固件
+
+- Windows 下按住 ESC 插入数据线，系统会挂载新的磁盘分区，将固件更名为 `HHKB_BLE.BIN` （大小写无关）然后copy 进分区中覆盖原文件即可刷入
+- macOS 下按住 ESC 插入数据线，然后：
+  - 通过`diskutil list` 确认键盘是在哪个分区
+  - 使用 `diskutil umount /Volumes/HHKB_BLE` 先卸载键盘，
+  - 使用 `sudo dd if=/path/to/vial-qmk-v5/ydkb_hhkb_ble_vial.bin of=/dev/diskX seek=4` 刷写固件
+
+## 快捷键列表
+键盘设置了四层，层号0~3，0 层为默认层，一层为多功能层，通过 FN 按下时激活，内容根 HHKB 默认一样没有变。2 层为 Windows 层，3 层为 Mac 层。其中 1-2-3 层也可以通过 `右Alt`+1/2/3 数字键长期激活，短按 `右Alt` 也会回到 0 层。
+
+|平台        | 按键        | 映射按键      | 含义        |
+|:-----------|:------------|:--------------|:------------|
+|BOTH        | LCTRL-H     | LEFT          |方向左      | 
+|BOTH        | LCTRL-J     | DOWN          |方向下      | 
+|BOTH        | LCTRL-K     | UP            |方向上      | 
+|BOTH        | LCTRL-L     | RIGHT         |方向右      | 
+|BOTH        | LALT-H      | LCTL-LEFT     |向左一个单词  | 
+|BOTH        | LALTL-L     | LCTL-RIGHT    |向右一个单词      | 
+|BOTH        | LSHIFT-ESC  | ~             |波浪号     | 
+|BOTH        | LCTL-ESC    | \`            |反引号         | 
+|BOTH        | LSHIFT-Back | \|            |中竖线     | 
+|BOTH        | LCTL-Back   | \\            |反斜杠     | 
+|BOTH        | LCTRL-E     | END           |END     | 
+|BOTH        | LCTRL-"     | Backspace     |退格删除 |
+|BOTH        | LCTRL-;     | Return         |回车      | 
+|BOTH        | LCTRL-B     | Backspace     |退格删除      | 
+|BOTH        | LALT-ESC    | LALT-\`       |ALT-反引号，我主要用来激活 mac 下的 iterm      | 
+|BOTH        | LALT-U      | Page UP       |上翻页    |
+|BOTH        | LALT-D      | Page DOWN     |下翻页    |
+|BOTH        | LALT-J      | Page UP       |上翻页    |
+|BOTH        | LALT-K      | Page DOWN     |下翻页    |
+|BOTH        | LCTL-D      | Delece        |删除当前字符    |
+|macOS       | LCTL-R      | CMD-R         |浏览器刷新      | 
+|Windows     | LCTRL-A     | HOME          |行首，mac下本来就是这个  | 
+|Windows     | LWIN-A      | LCTL-A        |全选,保持跟 mac 相同     |
+|Windows     | LCTRL-Q     | LALT-F4       |关闭当前应用程序   |
+|Windows     | LCTRL-N     | F2            |重命名当前文件 |
+|Windows     | LWIN-C      | LCTL-C        |复制       |
+|Windows     | LWIN-V      | LCTL-V        |粘贴       |
